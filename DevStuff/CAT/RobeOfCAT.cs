ed0e2cfccc6339c9762be325bd41ef3fc25b4f43
TheLuxure <65787578+TheLuxure@users.noreply.github.com>
2020-05-23 01:20:55 +0500

    Create RobeOfCAT.cs

---
+++ DevStuff/CAT/RobeOfCAT.cs
@@ -0,0 +1,37 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace ElementalHearts.DevStuff.CAT
+{
+	[AutoloadEquip(EquipType.Body)]
+	public class RobeOfCAT : ModItem
+	{
+		public override void SetStaticDefaults() {
+			base.SetStaticDefaults();
+			DisplayName.SetDefault("Robeof");
+			Tooltip.SetDefault("'Great for impersonating devs!'");
+		}
+
+		public override void SetDefaults() {
+			item.width = 18;
+			item.height = 18;
+			item.rare = 9;
+			item.vanity = true;
+		}
+
+		public override void UpdateEquip(Player player) {
+			if (player.name == "CAT") {
+				player.buffImmune[BuffID.OnFire] = true;
+				player.statLifeMax2 += 20;
+				player.maxMinions++;
+			}
+			if (player.name == "AstralCat") {
+				player.buffImmune[BuffID.OnFire] = true;
+				player.statLifeMax2 += 20;
+				player.maxMinions++;
+			}
+		}
+	}
+}
