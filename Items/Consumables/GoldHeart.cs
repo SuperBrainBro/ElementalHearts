c5a24d65283f6c897792a449fc7d1a35a05b247b
TheLuxure <65787578+TheLuxure@users.noreply.github.com>
2020-05-23 12:17:30 +0500

    Create GoldHeart.cs

---
+++ Items/Consumables/GoldHeart.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace ElementalHearts.Items.Consumables
+{
+	internal class GoldHeart : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("Permanently increases maximum life by 6");
+			DisplayName.SetDefault("Gold Heart");
+		}
+
+		public override void SetDefaults() {
+			item.CloneDefaults(ItemID.LifeFruit);
+			item.rare = 0;
+			item.value = 0;
+		}
+
+		public override bool CanUseItem(Player player)
+		{
+			return player.statLifeMax >= 100 && player.GetModPlayer<ElementalHeartsPlayer>().GoldLife < 1;
+		}
+
+		public override bool UseItem(Player player) {
+			player.statLifeMax2 += 6;
+			player.statLife += 6;
+			if (Main.myPlayer == player.whoAmI) {
+				player.HealEffect(6, true);
+			}
+			player.GetModPlayer<ElementalHeartsPlayer>().GoldLife += 1;
+			return true;
+		}
+
+		public override void AddRecipes() {
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.GoldOre, 100);;
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this, 1);
+			recipe.AddRecipe();
+		}
+	}
+}
