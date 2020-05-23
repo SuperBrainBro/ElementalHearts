5936fbe08b61f3c4c9ff88622623df14af71c291
TheLuxure <65787578+TheLuxure@users.noreply.github.com>
2020-05-24 01:13:59 +0500

    Update BossBags.cs

--- Items/BossBags.cs
+++ Items/BossBags.cs
@@ -18,12 +18,14 @@ namespace ElementalHearts.Items
 						player.QuickSpawnItem(ItemType<MaskOfCAT>(), Main.rand.Next(1, 1));
 						player.QuickSpawnItem(ItemType<RobeOfCAT>(), Main.rand.Next(1, 1));
 						player.QuickSpawnItem(ItemType<WingsOfCAT>(), Main.rand.Next(1, 1));
-						if (Main.rand.NextBool(4)) {
+						if (Main.rand.NextBool(5)) {
 							player.QuickSpawnItem(ItemType<CatastrophicEdge>(), Main.rand.Next(1, 1));
-						} else if (Main.rand.NextBool(4)) {
+						} else if (Main.rand.NextBool(5)) {
 							player.QuickSpawnItem(ItemType<CATsThrow>(), Main.rand.Next(1, 1));
-						} else if (Main.rand.NextBool(4)) {
+						} else if (Main.rand.NextBool(5)) {
 							player.QuickSpawnItem(ItemType<HeartOfCAT>(), Main.rand.Next(1, 1));
+						} else if (Main.rand.NextBool(5)) {
+							player.QuickSpawnItem(ItemType<TyrantsTear>(), Main.rand.Next(1, 1));
 						} else {
 							player.QuickSpawnItem(ItemType<AstralStars>(), Main.rand.Next(1, 1));
 						}
