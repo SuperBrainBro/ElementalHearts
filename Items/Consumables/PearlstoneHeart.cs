3ed50d737d86b9f7d8d726cf9fb905d1dccca087
TheLuxure <65787578+TheLuxure@users.noreply.github.com>
2020-05-23 13:05:46 +0500

    Create PearlstoneHeart.cs

---
+++ Items/Consumables/PearlstoneHeart.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace ElementalHearts.Items.Consumables
+{
+	internal class PearlstoneHeart : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("Permanently increases maximum life by 5");
+			DisplayName.SetDefault("Pearltone Heart");
+		}
+
+		public override void SetDefaults() {
+			item.CloneDefaults(ItemID.LifeFruit);
+			item.rare = 0;
+			item.value = 0;
+		}
+
+		public override bool CanUseItem(Player player)
+		{
+			return player.statLifeMax >= 100 && player.GetModPlayer<ElementalHeartsPlayer>().PearlstoneLife < 1;
+		}
+
+		public override bool UseItem(Player player) {
+			player.statLifeMax2 += 5;
+			player.statLife += 5;
+			if (Main.myPlayer == player.whoAmI) {
+				player.HealEffect(5, true);
+			}
+			player.GetModPlayer<ElementalHeartsPlayer>().PearlstoneLife += 1;
+			return true;
+		}
+
+		public override void AddRecipes() {
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(117, 100);;
+			recipe.AddTile(TileID.Furnaces);
+			recipe.SetResult(this, 1);
+			recipe.AddRecipe();
+		}
+	}
+}
