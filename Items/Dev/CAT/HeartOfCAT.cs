b6e3d7d5c5c39c38403f01525d23a3549d0aecb8
TheLuxure <65787578+TheLuxure@users.noreply.github.com>
2020-05-23 11:51:18 +0500

    Create HeartOfCAT.cs

---
+++ Items/Dev/CAT/HeartOfCAT.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace ElementalHearts.Items.Consumables
+{
+	internal class HeartOfCAT : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("Permanently increases maximum life by 1");
+			DisplayName.SetDefault("Heartof");
+		}
+
+		public override void SetDefaults() {
+			item.CloneDefaults(ItemID.LifeFruit);
+			item.rare = 9;
+			item.value = 0;
+		}
+
+		public override bool CanUseItem(Player player)
+		{
+			return player.statLifeMax >= 100 && player.GetModPlayer<ElementalHeartsPlayer>().HeartOfCAT < 1;
+		}
+
+		public override bool UseItem(Player player) {
+			player.statLifeMax2 += 1;
+			player.statLife += 1;
+			if (Main.myPlayer == player.whoAmI) {
+				player.HealEffect(1, true);
+			}
+			player.GetModPlayer<ElementalHeartsPlayer>().HeartOfCAT += 1;
+			return true;
+		}
+	}
+}
