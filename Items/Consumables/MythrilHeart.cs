1e356a8dbb7b4b5181cd7e4e199e9927ac3986e7
TheLuxure <65787578+TheLuxure@users.noreply.github.com>
2020-05-23 14:01:26 +0500

    Create MythrilHeart.cs

---
+++ Items/Consumables/MythrilHeart.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace ElementalHearts.Items.Consumables
+{
+	internal class MythrilHeart : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("Permanently increases maximum life by 10");
+			DisplayName.SetDefault("Mythril Heart");
+		}
+
+		public override void SetDefaults() {
+			item.CloneDefaults(ItemID.LifeFruit);
+			item.rare = 3;
+			item.value = 0;
+		}
+
+		public override bool CanUseItem(Player player)
+		{
+			return player.statLifeMax >= 100 && player.GetModPlayer<ElementalHeartsPlayer>().MythrilLife < 1;
+		}
+
+		public override bool UseItem(Player player) {
+			player.statLifeMax2 += 10;
+			player.statLife += 10;
+			if (Main.myPlayer == player.whoAmI) {
+				player.HealEffect(10, true);
+			}
+			player.GetModPlayer<ElementalHeartsPlayer>().MythrilLife += 1;
+			return true;
+		}
+
+		public override void AddRecipes() {
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(365, 100);;
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this, 1);
+			recipe.AddRecipe();
+		}
+	}
+}
