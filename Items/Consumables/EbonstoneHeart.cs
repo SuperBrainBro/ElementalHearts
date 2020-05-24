68c3d1f4d2cde5f1de5707c8eb1eb4e848ee8b43
SuperBrainBro <superbrainstormers@gmail.com>
2020-05-24 11:19:29 -0700

    crimstone and ebonstone

---
+++ Items/Consumables/EbonstoneHeart.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace ElementalHearts.Items.Consumables
+{
+	internal class EbonstoneHeart : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("Permanently increases maximum life by 1");
+			DisplayName.SetDefault("Ebonstone Heart");
+		}
+
+		public override void SetDefaults() {
+			item.CloneDefaults(ItemID.LifeFruit);
+			item.rare = ItemRarityID.White;
+			item.value = 0;
+		}
+
+		public override bool CanUseItem(Player player) {
+			return player.statLifeMax >= 100 && player.GetModPlayer<ElementalHeartsPlayer>().EbonstoneLife <
+				   ElementalHeartsPlayer.maxEbonstoneLife;
+		}
+
+		public override bool UseItem(Player player) {
+			player.statLifeMax2 += 1;
+			player.statLife += 1;
+			if (Main.myPlayer == player.whoAmI) {
+				player.HealEffect(1, true);
+			}
+			player.GetModPlayer<ElementalHeartsPlayer>().EbonstoneLife += 1;
+			return true;
+		}
+
+		public override void AddRecipes() {
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.EbonstoneBlock, 100);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this, 1);
+			recipe.AddRecipe();
+		}
+	}
+}
