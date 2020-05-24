0c361acd98137b0c260e85ed36d930acdae91e31
SuperBrainBro <superbrainstormers@gmail.com>
2020-05-24 11:00:35 -0700

    mushroom heart

--- Items/Consumables/MushroomHeart.cs
+++ Items/Consumables/MushroomHeart.cs
@@ -6,38 +6,39 @@ using static Terraria.ModLoader.ModContent;
 
 namespace ElementalHearts.Items.Consumables
 {
-	internal class MeteoriteHeart : ModItem
+	internal class MushroomHeart : ModItem
 	{
 		public override void SetStaticDefaults() {
-			Tooltip.SetDefault("Permanently increases maximum life by 8");
-			DisplayName.SetDefault("Meteorite Heart");
+			Tooltip.SetDefault("Permanently increases maximum life by 3");
+			DisplayName.SetDefault("Mushroom Heart");
 		}
 
 		public override void SetDefaults() {
 			item.CloneDefaults(ItemID.LifeFruit);
-			item.rare = ItemRarityID.Green;
+			item.rare = ItemRarityID.White;
 			item.value = 0;
 		}
 
 		public override bool CanUseItem(Player player) {
-			return player.statLifeMax >= 100 && player.GetModPlayer<ElementalHeartsPlayer>().MeteoriteLife <
-				   ElementalHeartsPlayer.maxMeteoriteLife;
+			return player.statLifeMax >= 100 && player.GetModPlayer<ElementalHeartsPlayer>().MushroomLife <
+				   ElementalHeartsPlayer.maxMushroomLife;
 		}
 
 		public override bool UseItem(Player player) {
-			player.statLifeMax2 += 8;
-			player.statLife += 8;
+			player.statLifeMax2 += 3;
+			player.statLife += 3;
 			if (Main.myPlayer == player.whoAmI) {
-				player.HealEffect(8, true);
+				player.HealEffect(3, true);
 			}
-			player.GetModPlayer<ElementalHeartsPlayer>().MeteoriteLife += 1;
+			player.GetModPlayer<ElementalHeartsPlayer>().MushroomLife += 1;
 			return true;
 		}
 
 		public override void AddRecipes() {
 			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.Meteorite, 100);;
-			recipe.AddTile(TileID.Anvils);
+			recipe.AddIngredient(ItemID.Mushroom, 50);
+			recipe.AddIngredient(ItemID.GlowingMushroom, 50);
+			recipe.AddTile(TileID.WorkBenches);
 			recipe.SetResult(this, 1);
 			recipe.AddRecipe();
 		}
