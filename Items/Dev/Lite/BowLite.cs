c5ce235091ee111c90962fbc669fb9bbbcbbc8ad
SuperBrainBro <superbrainstormers@gmail.com>
2020-05-24 20:13:14 -0700

    Update BowLite.cs

--- Items/Dev/Lite/BowLite.cs
+++ Items/Dev/Lite/BowLite.cs
@@ -34,7 +34,7 @@ namespace ElementalHearts.Items.Dev.Lite
 			item.knockBack = 1.5f;
 			item.rare = ItemRarityID.Cyan;
 		}
-		//est for gi
+		//st for gi
 		public override void OnConsumeAmmo(Player player)
 		{
 			if (Main.rand.NextBool(2))
