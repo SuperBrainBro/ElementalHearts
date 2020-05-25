446d9ebdbf0df50c61c92a21f52423a101898843
SuperBrainBro <superbrainstormers@gmail.com>
2020-05-25 01:25:31 -0700

    lots of boss code

--- NPCs/Bosses/MenacingHeart/MenacingHeart.cs
+++ NPCs/Bosses/MenacingHeart/MenacingHeart.cs
@@ -34,12 +34,56 @@ namespace ElementalHearts.NPCs.Bosses.MenacingHeart
         }
         public override void AI()
         {
-            if (npc.ai[1] <= 0)
+            //This float, bossPhaseHealth, is the amount of health divided by 4. Since there are 4 phases, there are 4 equal amounts. (See below code for example)
+             float bossPhaseHealth;
+            bossPhaseHealth = npc.lifeMax / 4;
+            
+            if (npc.life > bossPhaseHealth * 3)
+            {
+                //Set phase to 1.
+                npc.ai[0] = 1;
+            }
+            else if (npc.life > bossPhaseHealth * 2)
+            {
+                //Set phase to 2.
+                npc.ai[0] = 2;
+            }
+            else if (npc.life > bossPhaseHealth * 1)
+            {
+                //Set phase to 3.
+                npc.ai[0] = 2;
+            }
+            else if (npc.life > bossPhaseHealth * 0)
+            {
+                //Set phase to 4.
+                npc.ai[0] = 4;
+            }
+
+            //npc.ai[0] is the current phase. So the below code, checks for if it is phase 1. If it is in phase 1, then npc.ai[1] goes up (which is the timer for phase 10
+            if (npc.ai[0] == 1)
+            {
+                npc.ai[1]++;
+            }
+            else if (npc.ai[0] == 1)
+            {
+                npc.ai[1]++;
+            }
+            else if (npc.ai[0] == 1)
+            {
+                npc.ai[1]++;
+            }
+            else if (npc.ai[0] == 1)
+            {
+                npc.ai[1]++;
+            }
+
+            //This is the phase code, the one below, is the phase 1 code.
+            if (npc.ai[1] >= 20)
             {
                 npc.TargetClosest(true);
 
                 npc.position = Main.player[npc.target].position + new Vector2(100, 0);
-                npc.ai[1] = 20;
+                npc.ai[1] = 0;
             }
             base.AI();
         }
