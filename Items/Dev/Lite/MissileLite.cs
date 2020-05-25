594eaab3dd365d2922196472cf08e592443397e1
SuperBrainBro <superbrainstormers@gmail.com>
2020-05-24 19:56:32 -0700

    missilelite

---
+++ Items/Dev/Lite/MissileLite.cs
@@ -0,0 +1,34 @@
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace ElementalHearts.Items.Dev.Lite
+{
+	public class MissileLite : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("'Great for impersonating devs!' \nCasts a controllable missile \nHas a chance to shoot chlorophyte bullets in all directions which do high amounts of damage.");
+		}
+
+		public override void SetDefaults() {
+			item.damage = 200;
+			item.magic = true;
+			item.mana = 100;
+			item.width = 26;
+			item.height = 26;
+			item.useTime = 30;
+			item.useAnimation = 30;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.noMelee = true;
+			item.channel = true; //Channel so that you can held the weapon [Important]
+			item.knockBack = 16;
+			item.value = 0;
+			item.rare = ItemRarityID.Cyan;
+			item.UseSound = SoundID.Item9;
+			item.shoot = ProjectileType<MissileLiteProj>();
+			item.shootSpeed = 10f;
+		}
+	}
+}
