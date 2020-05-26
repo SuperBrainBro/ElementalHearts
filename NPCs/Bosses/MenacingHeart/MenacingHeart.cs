f105f60af6296dd99e3d0236fa05ce2382215e63
SuperBrainBro <superbrainstormers@gmail.com>
2020-05-26 11:14:06 -0700

    Update MenacingHeart.cs

--- NPCs/Bosses/MenacingHeart/MenacingHeart.cs
+++ NPCs/Bosses/MenacingHeart/MenacingHeart.cs
@@ -38,7 +38,7 @@ namespace ElementalHearts.NPCs.Bosses.MenacingHeart
             npc.HitSound = SoundID.Item35;
             npc.DeathSound = SoundID.Item25;
             music = mod.GetSoundSlot(SoundType.Music, "Sounds/Music/MenacingHeartBossMusic");
-            musicPriority = MusicPriority.BossMedium;
+            musicPriority = MusicPriority.BossHigh;
             npc.value = 100000;
             npc.buffImmune[BuffID.Confused] = true;
             npc.knockBackResist = 0f;
