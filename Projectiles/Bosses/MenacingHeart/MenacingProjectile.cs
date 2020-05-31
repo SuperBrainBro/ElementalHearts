7469823004e7d0a50db2bb99b9d61060dd5d1189
SuperBrainBro <superbrainstormers@gmail.com>
2020-05-31 12:56:10 -0700

    Update MenacingProjectile.cs

--- Projectiles/Bosses/MenacingHeart/MenacingProjectile.cs
+++ Projectiles/Bosses/MenacingHeart/MenacingProjectile.cs
@@ -16,30 +16,64 @@ namespace ElementalHearts.Projectiles.Bosses.MenacingHeart
 
 		public override void SetDefaults()
 		{
-			//projectile.aiStyle = 24;
 			projectile.penetrate = -1;
 			projectile.width = 44;
 			projectile.height = 44;
-			projectile.alpha =  150;
+			projectile.alpha =  0;
 			projectile.friendly = false;
 			projectile.hostile = true;
 			projectile.tileCollide = false;
 			projectile.ignoreWater = true;
+			projectile.timeLeft = 10;
 		}
 		public override void AI()
 		{
-			projectile.velocity *= 1.15f / 60f;
-			base.AI();
+			if (projectile.localAI[0] == 0f)
+			{
+				AdjustMagnitude(ref projectile.velocity);
+				projectile.localAI[0] = 1f;
+			}
+			Vector2 move = Vector2.Zero;
+			float distance = 400f;
+			bool target = false;
+			for (int k = 0; k < Main.maxPlayers; k++)
+			{
+				if (Main.player[k].active && !Main.player[k].dead)
+				{
+					Vector2 newMove = Main.player[k].Center;
+					float distanceTo = (float)Math.Sqrt(newMove.X * newMove.X + newMove.Y * newMove.Y);
+					if (distanceTo < distance)
+					{
+						move = newMove;
+						distance = distanceTo;
+						target = true;
+					}
+				}
+			}
+			if (target)
+			{
+				AdjustMagnitude(ref move);
+				projectile.velocity = (10 * projectile.velocity + move) / 11f;
+				AdjustMagnitude(ref projectile.velocity);
+			}
+			if (projectile.alpha == 0)
+			{
+				int dust1 = Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.Blood, projectile.velocity.X, projectile.velocity.Y, 0, Color.Red, 1);
+				int dust2 = Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.Blood, projectile.velocity.X, projectile.velocity.Y, 0, Color.Black, 1);
+				int dust3 = Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.Blood, projectile.velocity.X, projectile.velocity.Y, 0, Color.White, 1);
+				Main.dust[dust1].velocity /= 2f;
+				Main.dust[dust2].velocity /= 2f;
+				Main.dust[dust3].velocity /= 2f;
+			}
 		}
-		public override void PostAI()
+
+		private void AdjustMagnitude(ref Vector2 vector)
 		{
-			int dust1 = Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.Blood, projectile.velocity.X, projectile.velocity.Y, 0, Color.Red, 1);
-			int dust2 = Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.Blood, projectile.velocity.X, projectile.velocity.Y, 0, Color.Black, 1);
-			int dust3 = Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.Blood, projectile.velocity.X, projectile.velocity.Y, 0, Color.White, 1);
-			Main.dust[dust1].velocity /= 2f;
-			Main.dust[dust2].velocity /= 2f;
-			Main.dust[dust3].velocity /= 2f;
-			base.PostAI();
+			float magnitude = (float)Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+			if (magnitude > 6f)
+			{
+				vector *= 6f / magnitude;
+			}
 		}
 		public override Color? GetAlpha(Color lightColor)
 		{
