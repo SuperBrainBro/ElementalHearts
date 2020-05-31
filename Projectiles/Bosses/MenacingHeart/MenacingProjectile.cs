e8e3e165c26672b8c1618ea818fc5aa623f8086c
SuperBrainBro <superbrainstormers@gmail.com>
2020-05-31 12:59:17 -0700

    Update MenacingProjectile.cs

--- Projectiles/Bosses/MenacingHeart/MenacingProjectile.cs
+++ Projectiles/Bosses/MenacingHeart/MenacingProjectile.cs
@@ -9,11 +9,6 @@ namespace ElementalHearts.Projectiles.Bosses.MenacingHeart
 {
 	public class MenacingProjectile : ModProjectile
 	{
-		//public override void SetStaticDefaults()
-		//{
-			//ProjectileID.Sets.Homing[projectile.type] = true;
-		//}
-
 		public override void SetDefaults()
 		{
 			projectile.penetrate = -1;
@@ -28,34 +23,7 @@ namespace ElementalHearts.Projectiles.Bosses.MenacingHeart
 		}
 		public override void AI()
 		{
-			if (projectile.localAI[0] == 0f)
-			{
-				AdjustMagnitude(ref projectile.velocity);
-				projectile.localAI[0] = 1f;
-			}
-			Vector2 move = Vector2.Zero;
-			float distance = 400f;
-			bool target = false;
-			for (int k = 0; k < Main.maxPlayers; k++)
-			{
-				if (Main.player[k].active && !Main.player[k].dead)
-				{
-					Vector2 newMove = Main.player[k].Center;
-					float distanceTo = (float)Math.Sqrt(newMove.X * newMove.X + newMove.Y * newMove.Y);
-					if (distanceTo < distance)
-					{
-						move = newMove;
-						distance = distanceTo;
-						target = true;
-					}
-				}
-			}
-			if (target)
-			{
-				AdjustMagnitude(ref move);
-				projectile.velocity = (10 * projectile.velocity + move) / 11f;
-				AdjustMagnitude(ref projectile.velocity);
-			}
+			projectile.velocity *= 1.01f;
 			if (projectile.alpha == 0)
 			{
 				int dust1 = Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.Blood, projectile.velocity.X, projectile.velocity.Y, 0, Color.Red, 1);
@@ -66,18 +34,9 @@ namespace ElementalHearts.Projectiles.Bosses.MenacingHeart
 				Main.dust[dust3].velocity /= 2f;
 			}
 		}
-
-		private void AdjustMagnitude(ref Vector2 vector)
-		{
-			float magnitude = (float)Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
-			if (magnitude > 6f)
-			{
-				vector *= 6f / magnitude;
-			}
-		}
-		public override Color? GetAlpha(Color lightColor)
-		{
-			return Main.DiscoColor;
-		}
+		//public override Color? GetAlpha(Color lightColor)
+		//{
+			//return Main.DiscoColor;
+		//}
 	}
 }
