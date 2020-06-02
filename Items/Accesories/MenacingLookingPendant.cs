681ed4730d5dfd109644d8b3d1c3c4871b3110ea
SuperBrainBro <superbrainstormers@gmail.com>
2020-06-02 11:21:20 -0700

    menaicng looking endant

---
+++ Items/Accesories/MenacingLookingPendant.cs
@@ -0,0 +1,34 @@
+﻿using MonoMod.Cil;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+using static Mono.Cecil.Cil.OpCodes;
+
+namespace ElementalHearts.Items.Accessories
+{
+	//[AutoloadEquip(EquipType.Front)]
+	class MenacingLookingPendant : ModItem
+	{
+        public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Menacing Looking Pendant");
+			Tooltip.SetDefault("Increases life regen by 5. \nIncreases HP by 25");
+		}
+
+		public override void SetDefaults()
+		{
+			//sbyte realBackSlot = item.backSlot;
+			item.CloneDefaults(ItemID.PanicNecklace);
+			item.value = Item.sellPrice(0, 5, 0, 0);
+			//item.backSlot = realBackSlot;
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual)
+		{
+			player.lifeRegen += 5;
+			player.statLifeMax2 += 25;
+		}
+	}
+}
