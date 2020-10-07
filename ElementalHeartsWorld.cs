3200ad3e738c04d1f68d252480441c5a244fc0aa
SuperBrainBro <superbrainstormers@gmail.com>
2020-10-06 18:30:03 -0700

    ores

--- ElementalHeartsWorld.cs
+++ ElementalHeartsWorld.cs
@@ -70,15 +70,27 @@ namespace ElementalHearts
 
         public override void PostWorldGen()
         {
-
-            for (int i = 0; i < Main.maxTilesX / 900; i++)       //900 is how many biomes. the bigger is the number = less biomes
+            //Generation
+            for (int i = 0; i < Main.maxTilesX / 600; i++)       //900 is how many biomes. the bigger is the number = less biomes
             {
-                int X = WorldGen.genRand.Next(1, Main.maxTilesX - 300);
-                int Y = WorldGen.genRand.Next((int)WorldGen.rockLayer - 100, Main.maxTilesY - 200);
+                int X = WorldGen.genRand.Next(1, Main.maxTilesX - 100);
+                int Y = WorldGen.genRand.Next((int)WorldGen.rockLayer - 750, Main.maxTilesY - 100);
                 int TileType = TileID.PinkSlimeBlock;     //this is the tile u want to use for the biome , if u want to use a vanilla tile then its int TileType = 56; 56 is obsidian block
 
-                WorldGen.TileRunner(X, Y, 350, WorldGen.genRand.Next(100, 200), TileType, false, 0f, 0f, true, true);  //350 is how big is the biome     100, 200 this changes how random it looks.
+                WorldGen.TileRunner(X, Y, 100, WorldGen.genRand.Next(50, 100), TileType, false, 0f, 0f, true, true);  //350 is how big is the biome     100, 200 this changes how random it looks.
 
+                //Ore
+                for (int k = 0; k < 600; k++)                     //750 is the ore spawn rate. the bigger is the number = more ore spawns
+                {
+                    int Xo = X + Main.rand.Next(-240, 240);
+                    int Yo = Y + Main.rand.Next(-240, 240);
+                    if (Main.tile[Xo, Yo].type == TileID.RedBrick)   //this is the tile where the ore will spawn
+                    {
+
+
+                        WorldGen.TileRunner(Xo, Yo, (double)WorldGen.genRand.Next(5, 10), WorldGen.genRand.Next(5, 10), mod.TileType("CustomOreTile"), false, 0f, 0f, false, true);  //   5, 10 is how big is the ore veins.
+                    }
+                }
             }
         }
     }
