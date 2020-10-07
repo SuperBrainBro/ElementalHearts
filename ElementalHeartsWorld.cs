8b914eac61a0a55aae290b9883bbf13673e4b141
SuperBrainBro <superbrainstormers@gmail.com>
2020-10-06 17:57:28 -0700

    setup biome generation

--- ElementalHeartsWorld.cs
+++ ElementalHeartsWorld.cs
@@ -1,7 +1,12 @@
-using System.Collections.Generic;
 using System.IO;
+using System.Collections.Generic;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.World.Generation;
+using Microsoft.Xna.Framework;
+using Terraria.GameContent.Generation;
+using System.Linq;
 using Terraria.ModLoader.IO;
 
 namespace ElementalHearts
@@ -61,6 +66,30 @@ namespace ElementalHearts
             BitsByte flags = reader.ReadByte();
             downedMenacingHeart = flags[0];
 
+        }
+        
+        
+        public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight)
+        {
+            int genIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Shinies"));
+            if (genIndex == -1)
+            {
+                return;
+            }
+            tasks.Insert(genIndex + 1, new PassLegacy("Custom Biome", delegate (GenerationProgress progress)
+            {
+                progress.Message = "Custom Biome Progress";
+                for (int i = 0; i < Main.maxTilesX / 900; i++)       //900 is how many biomes. the bigger is the number = less biomes
+                {
+                    int X = WorldGen.genRand.Next(1, Main.maxTilesX - 300);
+                    int Y = WorldGen.genRand.Next((int)WorldGen.rockLayer - 100, Main.maxTilesY - 200);
+                    int TileType = mod.TileType("CustomTileBlock");     //this is the tile u want to use for the biome , if u want to use a vanilla tile then its int TileType = 56; 56 is obsidian block
+
+                    WorldGen.TileRunner(X, Y, 350, WorldGen.genRand.Next(100, 200), TileType, false, 0f, 0f, true, true);  //350 is how big is the biome     100, 200 this changes how random it looks.
+
+                }
+
+            }));
         }
     }
 }
