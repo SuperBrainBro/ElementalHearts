d8a8624a565a515c04e2c1f50524fd010e34d6c9
SuperBrainBro <superbrainstormers@gmail.com>
2020-10-07 13:52:43 -0700

    renaming

--- Items/Walls/CrystalWall.cs
+++ Items/Walls/CrystalWall.cs
@@ -3,7 +3,7 @@ using Terraria.ModLoader;
 
 namespace ElementalHearts.Items.Walls
 {
-    public class CrystalWalls : ModItem
+    public class CrystalWallItem : ModItem
     {
         public override void SetStaticDefaults()
         {
