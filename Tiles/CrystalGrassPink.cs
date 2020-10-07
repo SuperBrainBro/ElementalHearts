adfa0f7773b08271ee5ab67b1d5be0f7a376e054
SuperBrainBro <superbrainstormers@gmail.com>
2020-10-06 20:25:33 -0700

    debug

--- Tiles/CrystalGrassPink.cs
+++ Tiles/CrystalGrassPink.cs
@@ -10,7 +10,7 @@ namespace ElementalHearts.Tiles
         public override void SetDefaults()
         {
             Main.tileSolid[Type] = true;
-            Main.tileMergeDirt[Type] = false;
+            Main.tileMergeDirt[Type] = true;
             Main.tileLighted[Type] = true;
             Main.tileLavaDeath[Type] = true;
 
