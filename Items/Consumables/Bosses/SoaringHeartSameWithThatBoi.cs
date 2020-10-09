c87a0bfde39e137630f7f7fe3b46f06d9e5a4e02
TheLuxure <65787578+TheLuxure@users.noreply.github.com>
2020-10-09 23:11:08 +0500

    two last boss hearts, i hope

---
+++ Items/Consumables/Bosses/SoaringHeartSameWithThatBoi.cs
@@ -0,0 +1,46 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ElementalHearts.Items.Consumables.Bosses
+{
+    internal class SoaringHeartSameWithThatBoi : ModItem
+    {
+        public override bool Autoload(ref string name)
+        {
+            return Main.LocalPlayer.name != "Fox" || Main.LocalPlayer.name != "Lite";
+        }
+
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Permanently increases maximum life by 10\nThat heart visible only for Devs, i guess");
+            DisplayName.SetDefault("Soaring Heart");
+        }
+
+        public override void SetDefaults()
+        {
+            item.CloneDefaults(ItemID.LifeFruit);
+            item.rare = ItemRarityID.Expert;
+            item.value = 0;
+            item.expert = true;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            return player.statLifeMax >= 100 && player.GetModPlayer<ElementalHeartsPlayer>().SoaringLife <
+                   ModContent.GetInstance<ElementalHeartsConfig>().MaxElementalHeartConfig;
+        }
+
+        public override bool UseItem(Player player)
+        {
+            player.statLifeMax2 += 10;
+            player.statLife += 10;
+            if (Main.myPlayer == player.whoAmI)
+            {
+                player.HealEffect(10, true);
+            }
+            player.GetModPlayer<ElementalHeartsPlayer>().SoaringLife += 1;
+            return true;
+        }
+    }
+}
