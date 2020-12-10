78875cc9cb27616a3017df895bb684d70daf59e1
TheLuxure <65787578+TheLuxure@users.noreply.github.com>
2020-12-11 00:26:20 +0500

    ach

---
+++ Achievements/HeartCrusherAchievement.cs
@@ -0,0 +1,24 @@
+﻿using ElementalHearts.NPCs.Bosses.MenacingHeart;
+using Terraria.Achievements;
+using Terraria.GameContent.Achievements;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WebmilioCommons.Achievements;
+
+namespace ElementalHearts.Achievements
+{
+    public class HeartCrusherAchievement : ModAchievement
+    {
+        public HeartCrusherAchievement() : base("Heart Crusher", "Defeat the Menacing Heart",
+            AchievementCategory.Slayer)
+        { }
+
+        public override void SetDefaults()
+        {
+            AddConditions(NPCKilledCondition.Create((short)(int)ModContent.NPCType<MenacingHeart>()));
+        }
+
+        private Mod mod = ModLoader.GetMod("WebmilioCommons");
+        public override bool Autoload { get => mod != null; protected set => mod = Mod; }
+    }
+}
