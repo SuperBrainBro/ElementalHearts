fd94015805a13ecdad57670415bad5a197f091ac
Vincent Jenei <vincentjenei@gmail.com>
2021-04-02 12:51:56 -0700

    s

---
+++ Achievements/HeartCrusherAchievement.cs
@@ -0,0 +1,25 @@
+﻿using ElementalHearts.NPCs.Bosses.MenacingHeart;
+using Terraria.Achievements;
+using Terraria.GameContent.Achievements;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WebmilioCommons.Achievements;
+
+namespace ElementalHearts.Achievements
+{
+    public class HeartCrusherAchievement : ModAchievement
+    {
+        public HeartCrusherAchievement() : base("Heart Crusher", "Defeat the Menacing Heart",
+            AchievementCategory.Slayer)
+        { }
+
+        public override void SetDefaults()
+        {
+            AddCompletionFlag();
+            AddConditions(NPCKilledCondition.Create((short)ModContent.NPCType<MenacingHeart>()));
+        }
+
+        private Mod mod = ModLoader.GetMod("WebmilioCommons");
+        public override bool Autoload { get => mod != null; protected set => mod = Mod; }
+    }
+}
